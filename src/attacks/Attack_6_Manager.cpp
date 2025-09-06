2d52eab0ce7339ba5dc2f39de94b561e0e8e1158
Xuan Nghia <nghiagmt@gmail.com>
2025-09-06 01:24:23 -0500

    Added placeholder for Attack_6_Manager

---
+++ src/attacks/Attack_6_Manager.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Attack_6_Manager.h"
+
+Attack_6_Manager::Attack_6_Manager() {}
+
+void Attack_6_Manager::update() {}
+void Attack_6_Manager::render() {}
+void Attack_6_Manager::ready_to_be_removed() {}
